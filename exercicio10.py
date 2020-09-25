b2569f7a8e563bed4e190a12735fb53a5dd179f8
Igor Barbosa Ramalho <60898867+igor007-cyber@users.noreply.github.com>
2020-09-25 11:34:49 -0300

    Update exercicio10.py

--- exercicio10.py
+++ exercicio10.py
@@ -2,6 +2,6 @@
 10) Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
 """
 C = float(input("digite um numero em graus Celsius: "))
-F = (9 * C + 160)/5
+F = (C * 9/5) + 32;
 
 print("o graus em Fahrenheit: ", F)
